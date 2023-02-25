b1081c9f20b7e1fb5822d27db70a55f022af5039
Julien Abraham <julienabraham@gmail.com>
2023-02-25 09:53:59 -0500

    change url for dtatabase connect

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   client: "pg",
   connection: {
-    host: "postgres://jab:gYDSEDhVbeBCToNP9GgOEdhHjZNUDLjQ@dpg-cft1e8p4reb6ks61dq7g-a.ohio-postgres.render.com/swap_right",
+    host: "postgres://jab:gYDSEDhVbeBCToNP9GgOEdhHjZNUDLjQ@dpg-cft1e8p4reb6ks61dq7g-a/swap_right",
     user: "jab",
     password: process.env.PG_PASSWORD,
     database: "swap_right",
