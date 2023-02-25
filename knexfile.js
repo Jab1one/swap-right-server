1b0c04b5ba1137a4e4f3e05457e5263c8b86404d
Julien Abraham <julienabraham@gmail.com>
2023-02-25 09:38:44 -0500

    add pg knex

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 module.exports = {
-  client: "mysql",
+  client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
+    host: "postgres://jab:gYDSEDhVbeBCToNP9GgOEdhHjZNUDLjQ@dpg-cft1e8p4reb6ks61dq7g-a.ohio-postgres.render.com/swap_right",
+    user: "jab",
+    password: process.env.PG_PASSWORD,
     database: "swap_right",
     charset: "utf8",
   },
