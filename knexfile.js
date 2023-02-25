32c2e22a7849b4ab9fd4edf948fc520f22ac2add
Julien Abraham <julienabraham@gmail.com>
2023-02-25 18:10:48 -0500

    add port to knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@ module.exports = {
   client: "pg",
   connection: {
     host: process.env.DATA_URL,
+    port: 5432,
     user: "jab",
     password: process.env.PG_PASSWORD,
     database: "swap_right",
