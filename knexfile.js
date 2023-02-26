220c56303c9a9acee467d90510d56ef8b266610e
Julien Abraham <julienabraham@gmail.com>
2023-02-25 21:53:43 -0500

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,15 @@
 module.exports = {
   client: "pg",
-  connection: process.env.DATA_URL,
+  connection: {
+    connectionString: process.env.DATA_URL,
+    host : process.env.HOSTNAME,
+    port : 5432,
+    user : process.env.USERNAME,
+    password : process.env.DB_PASSWORD, database : process.env.DATABASE,
+    ssl: {rejectUnauthorized: false}
+  },
   seeds: {
     directory: "./seed_data",
   },
 };
+
