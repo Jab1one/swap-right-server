2255952c992f6bfcf5e947dfa54788f4ec22d09b
Julien Abraham <julienabraham@gmail.com>
2023-03-04 09:49:54 -0500

    adjust knex file

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,20 @@
 module.exports = {
-  // client: "pg",
-  client: "mysql",
+  client: "pg",
+  // client: "mysql",
   connection: {
-    // connectionString: process.env.DATA_URL,
-    // host : process.env.HOSTNAME,
-    // port : 5432,
-    // user : process.env.USERNAME,
-    // password : process.env.DB_PASSWORD, 
-    // database : process.env.DATABASE,
-    // ssl: false
-    // ssl: {rejectUnauthorized: false}
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "swap_right",
-    charset: "utf8"
+    connectionString: process.env.DATA_URL,
+    host : process.env.HOSTNAME,
+    port : 5432,
+    user : process.env.USERNAME,
+    password : process.env.DB_PASSWORD, 
+    database : process.env.DATABASE,
+    ssl: false,
+    ssl: {rejectUnauthorized: false}
+    // host: "127.0.0.1",
+    // user: "root",
+    // password: "rootroot",
+    // database: "swap_right",
+    // charset: "utf8"
 
   },
   seeds: {
